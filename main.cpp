8321535fe640b867929dc291d1c1c66bb7717a23
= <=>
2024-06-25 11:22:27 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include "lib/headers/cstring.h"
 
 
 
 int main() {
-	std::cout << "=== C++ Hand Made Functions ===" << std::endl;
+	std::cout << "=== C++ Learning Journey ===" << std::endl;
 
-    std::cout << wordCount("##One#iWord##") << std::endl;
 	return 0;
 }
